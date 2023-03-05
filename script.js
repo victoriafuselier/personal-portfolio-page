fbe8a3615f11273676a33f9e46d192aa224663db
Victoria Fuselier <vic.fuselier@gmail.com>
2023-03-05 11:21:35 -0600

    Fixed JS issues and updated html
    All about me li's now have a functional toggle show/hide button below... Still need to finish HTML update by getting rid of list within a list issue.

--- script.js
+++ script.js
@@ -2,36 +2,124 @@ console.log('Hello Victoria. Your file is linked');
 
 const moreButton1 = document.getElementById('more-button1');
 const moreButton2 = document.getElementById('more-button2');
+const moreButton3 = document.getElementById('more-button3');
+const moreButton4 = document.getElementById('more-button4');
+const moreButton5 = document.getElementById('more-button5');
+const moreButton6 = document.getElementById('more-button6');
+const moreButton7 = document.getElementById('more-button7');
+const moreButton8 = document.getElementById('more-button8');
+const moreButton9 = document.getElementById('more-button9');
+const moreButton10 = document.getElementById('more-button10');
+const moreButton11 = document.getElementById('more-button11');
 
 moreButton1.addEventListener('click', () => {
-    if (moreButton1.textContent = 'More') {
-        console.log('More');
-        const hobby1info = document.getElementById('hobby1info');
-            
+    const hobby1info = document.getElementById('hobby1info');
+    if (moreButton1.textContent === 'Read More') {    
         hobby1info.className = 'visible-info';
         moreButton1.textContent = 'Less';
-    } else if (moreButton1.textContent = 'Less') {
-        console.log('Less');
-
-        const hobby1info = document.getElementById('hobby1info');
-
-            hobby1info.className = 'hidden-info';
-            moreButton1.textContent = 'More';
+    } else if (moreButton1.textContent === 'Less') {
+        hobby1info.className = 'hidden-info';
+        moreButton1.textContent = 'Read More';
     }})
 
 moreButton2.addEventListener('click', () => {
-    if (moreButton2.textContent = 'More') {
-        console.log('More');
-        const hobby2info = document.getElementById('hobby2info');
-            
+    const hobby2info = document.getElementById('hobby2info');
+    if (moreButton2.textContent === 'Read More') {    
         hobby2info.className = 'visible-info';
         moreButton2.textContent = 'Less';
-    } else if (moreButton2.textContent = 'Less') {
-        console.log('Less');
+    } else if (moreButton2.textContent === 'Less') {
+        hobby2info.className = 'hidden-info';
+        moreButton2.textContent = 'Read More';
+    }})
 
-        const hobby2info = document.getElementById('hobby2info');
+moreButton3.addEventListener('click', () => {
+    const hobby3info = document.getElementById('hobby3info');
+    if (moreButton3.textContent === 'Read More') {    
+        hobby3info.className = 'visible-info';
+        moreButton3.textContent = 'Less';
+    } else if (moreButton3.textContent === 'Less') {
+        hobby3info.className = 'hidden-info';
+        moreButton3.textContent = 'Read More';
+    }})
 
-            hobby2info.className = 'hidden-info';
-            moreButton2.textContent = 'More';
-    }})  
+moreButton4.addEventListener('click', () => {
+    const hobby4info = document.getElementById('hobby4info');
+    if (moreButton4.textContent === 'Read More') {    
+        hobby4info.className = 'visible-info';
+        moreButton4.textContent = 'Less';
+    } else if (moreButton4.textContent === 'Less') {
+        hobby4info.className = 'hidden-info';
+        moreButton4.textContent = 'Read More';
+    }})
+
+moreButton5.addEventListener('click', () => {
+    const hobby5info = document.getElementById('hobby5info');
+    if (moreButton5.textContent === 'Read More') {    
+        hobby5info.className = 'visible-info';
+        moreButton5.textContent = 'Less';
+    } else if (moreButton5.textContent === 'Less') {
+        hobby5info.className = 'hidden-info';
+        moreButton5.textContent = 'Read More';
+    }})
+
+moreButton6.addEventListener('click', () => {
+    const hobby6info = document.getElementById('hobby6info');
+    if (moreButton6.textContent === 'Read More') {    
+        hobby6info.className = 'visible-info';
+        moreButton6.textContent = 'Less';
+    } else if (moreButton6.textContent === 'Less') {
+        hobby6info.className = 'hidden-info';
+        moreButton6.textContent = 'Read More';
+    }})
+    
+moreButton7.addEventListener('click', () => {
+    const hobby7info = document.getElementById('hobby7info');
+    if (moreButton7.textContent === 'Read More') {    
+        hobby7info.className = 'visible-info';
+        moreButton7.textContent = 'Less';
+    } else if (moreButton7.textContent === 'Less') {
+        hobby7info.className = 'hidden-info';
+        moreButton7.textContent = 'Read More';
+    }})
+
+moreButton8.addEventListener('click', () => {
+    const hobby8info = document.getElementById('hobby8info');
+    if (moreButton8.textContent === 'Read More') {    
+        hobby8info.className = 'visible-info';
+        moreButton8.textContent = 'Less';
+    } else if (moreButton8.textContent === 'Less') {
+        hobby8info.className = 'hidden-info';
+        moreButton8.textContent = 'Read More';
+    }})
+
+moreButton9.addEventListener('click', () => {
+    const hobby8info = document.getElementById('hobby9info');
+    if (moreButton9.textContent === 'Read More') {    
+        hobby9info.className = 'visible-info';
+        moreButton9.textContent = 'Less';
+    } else if (moreButton9.textContent === 'Less') {
+        hobby9info.className = 'hidden-info';
+        moreButton9.textContent = 'Read More';
+    }})
+
+moreButton10.addEventListener('click', () => {
+    const hobby10info = document.getElementById('hobby10info');
+    if (moreButton10.textContent === 'Read More') {    
+        hobby10info.className = 'visible-info';
+        moreButton10.textContent = 'Less';
+    } else if (moreButton10.textContent === 'Less') {
+        hobby10info.className = 'hidden-info';
+        moreButton10.textContent = 'Read More';
+    }})
+
+moreButton11.addEventListener('click', () => {
+    const hobby11info = document.getElementById('hobby11info');
+    if (moreButton11.textContent === 'Read More') {    
+        hobby11info.className = 'visible-info';
+        moreButton11.textContent = 'Less';
+    } else if (moreButton11.textContent === 'Less') {
+        hobby11info.className = 'hidden-info';
+        moreButton11.textContent = 'Read More';
+    }})
+
 
